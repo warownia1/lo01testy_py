dc4d04f2a1b2cfc1ebffedfee5770803caae92c0
unknown <warownia1@gmail.com>
2015-10-21 15:29:30 +0100

    Comment on models.

--- lo01testy/accounts/models.py
+++ lo01testy/accounts/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 from examination.models import Exam
 
 
+# may extend django.contrib.auth.models.User
 class Student(models.Model):
+    """Extension of a User model"""
     user = models.OneToOneField(User)
     rating = models.IntegerField(default=1500)
     
@@ -13,6 +15,13 @@ class Student(models.Model):
 
     
 class RegisterCode(models.Model):
+    """Codes required during registration of the user in the system.
+
+    During registration user is asked to enter one of the codes which is
+    uniquely generated for each student.
+    Code is deleted after being used.
+    """
+    # in the future, code will be used to identify students
     code = models.CharField(max_length=16)
     
   
--- lo01testy/examination/models.py
+++ lo01testy/examination/models.py
@@ -6,7 +6,9 @@ from .enums import QuestionType
 
 
 class Exam(models.Model):
+    """Table of all exams."""
     name = models.CharField(max_length=30)
+    # number of questions which student will be asked during the test.
     num_questions = models.IntegerField()
 
     def __str__(self):
@@ -14,6 +16,11 @@ class Exam(models.Model):
 
 
 class Group(models.Model):
+    """A group of students.
+
+    Exams are assigned to the group and all students which belong to that
+    group have access to the exam.
+    """
     name = models.CharField(max_length=40)
     exams = models.ManyToManyField(Exam, through='Assign')
     members = models.ManyToManyField(User)
@@ -23,15 +30,24 @@ class Group(models.Model):
 
 
 class Assign(models.Model):
+    """An intermediary table containing details of group-exam relation."""
     exam = models.ForeignKey(Exam)
     group = models.ForeignKey(Group)
+    # date due exam must be taken by the group
+    # in case of conflict, always the latest due date is considered.
     due_date = models.DateField(editable=True)
+    # date when exam was added to group
     creation_date = models.DateField(auto_now_add=True)
 
 
 class ExamCode(models.Model):
+    """Table of codes required to take the exam in grade mode.
+
+    During the assessed workshop, student inputs the code provided by teacher.
+    Code required for grade mode only."""
     exam = models.ForeignKey(Exam)
     code = models.CharField(max_length=10)
+    # expiration time of the code
     expiry_date = models.DateTimeField()
 
     def __str__(self):
@@ -39,6 +55,7 @@ class ExamCode(models.Model):
 
 
 class Question(models.Model):
+    """Table of questions assigned to a particular exam."""
     SINGLE_CHOICE = QuestionType.single_choice
     MULTIPLE_CHOICE = QuestionType.multiple_choice
     OPEN_ENDED = QuestionType.open_ended
@@ -54,6 +71,7 @@ class Question(models.Model):
         default=SINGLE_CHOICE
     )
     text = models.TextField()
+    # the difficulty (rating) of the question
     rating = models.IntegerField(default=1500)
 
     def __str__(self):
@@ -64,6 +82,7 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    """Table of answers assigned to a question."""
     question = models.ForeignKey(Question)
     text = models.CharField(max_length=100)
     is_correct = models.BooleanField()
@@ -76,9 +95,12 @@ class Answer(models.Model):
             
             
 class ExamRegister(models.Model):
+    """Collection of all exams already taken by students."""
     user = models.ForeignKey(User)
     exam = models.ForeignKey(Exam)
+    # date when exam was taken
     date = models.DateField(auto_now_add=True)
+    # rating of the user when was taking the exam
     user_rating = models.IntegerField()
     
     def __str__(self):
@@ -86,8 +108,13 @@ class ExamRegister(models.Model):
             self.date, self.exam.name, self.user.username)
     
     
+# this model is storing students' answers
+# it's user in open-ended questions rather than single/multiple choice
 class AnswerRegister(models.Model):
+    """Collection of answers given by students during exams"""
     exam_attempt = models.ForeignKey(ExamRegister)
     question = models.ForeignKey(Question)
+    # answer contains a json object with student's answer
     answer = models.CharField(max_length=25)
+    # whether questions was already graded
     graded = models.BooleanField(default=False)
